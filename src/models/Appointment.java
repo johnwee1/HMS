45d57ac01358e10c2be68b0edeb02ec8f9065c86
John Wee <91670254+johnwee1@users.noreply.github.com>
2024-11-06 17:50:10 +0800

    Squash merge appointments into main

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.UUID;
+
 /**
  * Public Schema class for the models.Appointment object.
  * the names of the columns are case-sensitive.
@@ -7,15 +9,30 @@ package models;
  */
 public class Appointment implements IdentifiedObject {
     public String id;
-    public String startTime;
-    public String endTime;
+    public String startTime; //format: DDMMYY:HH (in 24hr format)
+    public String endTime; //format: DDMMYY:HH (in 24hr format)
     public int appointmentType;
-    public int appointmentStatus;
-    public int patient_id;
-    public int doctor_id;
+    public int appointmentStatus; // 0: booked 1: avaliable 2: pending 3: completed
+    public String patient_id;
+    public String doctor_id;
     public boolean isPrescribed;
     public String diagnosis;
     public String prescription;
+
+    public Appointment(){};
+    public Appointment(String startTime, String endTime, int type, int status, String patient_id, String doctor_id){
+        this.id = UUID.randomUUID().toString();
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.appointmentType = type;
+        this.appointmentStatus = status;
+        this.patient_id = patient_id;
+        this.doctor_id = doctor_id;
+        this.isPrescribed = false;
+        this.diagnosis = "";
+        this.prescription = "";
+        // set prescription and diagnosis only after completion
+    }
 
     public String getID(){
         return id;
