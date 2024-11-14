53b453e6b6f97f0f833ca4d61c466e14a39fdd29
John Wee <91670254+johnwee1@users.noreply.github.com>
2024-11-15 02:16:57 +0800

    refactor mainmenu

---
+++ src/menus/MainMenu.java
@@ -0,0 +1,56 @@
+package menus;
+
+import repository.UserRepository;
+import models.User;
+import utils.PasswordHelper;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class MainMenu {
+    final static String filename = "test_users.csv";
+    final static String filepath = java.nio.file.Paths.get(System.getProperty("user.dir"),"test","resources", filename).toString();
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        final UserRepository userRepository = new UserRepository(filepath);
+
+        System.out.println("Hospital Management System");
+
+        System.out.println("Login with username:");
+        String username = s.next();
+        System.out.println("Password:");
+        String password = s.next();
+        User user;
+        while (true) {
+            try {
+                user = userRepository.getUserObject(username);
+                break;
+            } catch (RuntimeException e) {
+                System.out.println("user does not exist");
+            }
+        }
+        int attempts=0;
+        for (;attempts<5;attempts++) {
+            if (!PasswordHelper.verify(password, user.passwordHash)) {
+                System.out.println("Invalid password. Please try again.");
+            } else break;
+        }
+        if (attempts >=5) {
+            System.out.println("Login too many times. goodbye!");
+            exit(0); // exit program
+        }
+
+        System.out.println("Login successful!");
+        String role = userRepository.getUserRole(username);
+
+        //REPLACE WITH FACTORY
+        //switch cases for routing to specific CLIsn
+//        switch(role){
+//            case "patient" -> new PatientMenu().run();
+//            case "doctor" -> new DoctorMenu().run();
+//            case "pharmacist" -> new PharmacistMenu.run();
+//            case "admin" -> new AdminMenu.run();
+//            default -> System.out.println("Role unknown. Please try again.");
+    }
+}
